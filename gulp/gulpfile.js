b14d4732e5c3349270efd91e9e339721b13b4bc1
hikaruu <hikaruu@h-t.co.jp>
2015-12-18 14:28:27 +0900

    thpr (肝心のgulpfileわすれてた)

---
+++ gulp/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var typescript = require('gulp-typescript');
+var concat = require('gulp-concat');
+
+var typescriptProject = typescript.createProject({
+    target: "ES5",
+    removeComments: true,
+    sortOutput: true
+});
+
+gulp.task('ts-compile', function(){
+    gulp.src(['../src/ts/**/*.ts'])
+        .pipe(typescript(typescriptProject))
+        .js
+        .pipe(concat("main.js"))
+        .pipe(gulp.dest('../client/js/'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('../src/ts/**/*.ts', ['ts-compile']);
+});
+
+gulp.task('default', ['watch']);
